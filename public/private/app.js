d5784683d82296b43beafc5a28b5aa180380b749
Balays33 <40806995+Balays33@users.noreply.github.com>
2020-04-05 19:38:13 +0100

    update

---
+++ public/private/app.js
@@ -0,0 +1,115 @@
+
+console.log("Hello world!");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBq8V3pCR86GhE3XhdaCEQqczfnysZHUk",
+  authDomain: "shareandsplit-c1006.firebaseapp.com",
+  databaseURL: "https://shareandsplit-c1006.firebaseio.com",
+  projectId: "shareandsplit-c1006",
+  storageBucket: "shareandsplit-c1006.appspot.com",
+  messagingSenderId: "205797103800",
+  appId: "1:205797103800:web:3cff34316bed297ae57c4d",
+  measurementId: "G-X04ZQ3QW5L"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// Get a reference to the database service
+//var database = firebase.database();
+
+// get elements
+const preObject = document.getElementById('object');
+
+// create references
+const dbRefObject = firebase.database().ref().child('object');
+//const dbRefObject = firebase.database().ref("object/favNumber");
+
+//Sync object changes
+//dbRefObject.on('value', snap =>  console.log(snap.val()));
+dbRefObject.on('value', snap => { 
+  preObject.innerHTML = JSON.stringify(snap.val(), null, 3);
+});
+
+
+
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}
+
+// get elements
+const finalMoney = document.getElementById("finalPay");
+// create refeences
+const dbfinalMoney = firebase.database().ref().child('code');
+//Sync object changes
+dbfinalMoney.on('value',snap => console.log(snap.val()));
+
+
+
+function writeUserData(userId, name) {
+  console.log("working");
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+  });
+}
+
+
+const carList = document.querySelector('#car-list');
+
+function pushcustomer() {
+
+  console.log("test here",email);
+  window.alert("Thanks for the update personal details");
+  var firstname = document.getElementById("firstname").value;
+  var secondname = document.getElementById("secondname").value;
+  var phonenumber = document.getElementById("phonenumber").value;
+  
+  var address = document.getElementById("address").value;
+  var birthday = document.getElementById("birthday").value;
+  var comment = document.getElementById("comment").value;
+  console.log('send data up');
+  // Add a new document in collection "cities"
+  db.collection("customers").doc(email).set({
+      CustomerFirstName: firstname,
+      CustomerSecondName: secondname,
+      CustomerMobileNumber: phonenumber,
+      CustomerEmail: email,
+      CustomerAddress: address,
+      CustomerBday: birthday,
+      CustomerComments: comment
+})
+      .then(function () {
+          console.log("Document successfully written!");
+      })
+      .catch(function (error) {
+          console.error("Error writing document: ", error);
+      });
+
+}
+
+
+
+ function GenerateACode(length) {
+  console.log("Generate a Code");
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  document.getElementById("generatedNumber").innerHTML = result;
+  return result;
+}
+
+
+function sentCode(){
+  var getCode = document.getElementById("inputCode").value;
+  console.log(getCode);
+  //console.log("test");
+
+
+}
